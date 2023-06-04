570e1543e86e00231dc5247cb80e1f07e9ee05b1
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-06-04 14:43:06 +0200

    main loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,23 @@
 
 class Main {
     constructor() {
+        this.backgroundColor = '#8C8C8C';
+        this.fps = 60;
+    }
+
+    draw() {
+        drawRect(this.backgroundColor, [0, 0, _$canvas.width, _$canvas.height]);
+    }
+
+    update() {
+
     }
 
     run() {
-
+        // main loop
+        setInterval(() => {
+            this.update();
+            this.draw();
+        }, 1000 / this.fps);
     }
 }
