a8658b9c18a37edb99c1afb3a72e22676340c722
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-06-04 14:48:26 +0200

    node constructor and draw method

--- assets/js/node.js
+++ assets/js/node.js
@@ -1,7 +1,19 @@
 "use strict";
 
 class Node {
-    constructor() {
-        
+    static NEXT_ID = 0;
+    constructor(x, y) {
+        this.color = '#e3e3e3'
+        this.radius = 5;
+        this.pos = {
+            x: x,
+            y: y
+        }
+        this.id = Node.NEXT_ID;
+        Node.NEXT_ID += 1;
+    }
+
+    draw() {
+        drawCircle(this.color, [this.pos.x, this.pos.y], this.radius);
     }
 }
