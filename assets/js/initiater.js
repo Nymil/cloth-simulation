e346c552e1585cebb4070aa7cc1655fdef947069
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-06-04 12:46:43 +0200

    initiater done and dusted

--- assets/js/initiater.js
+++ assets/js/initiater.js
@@ -3,5 +3,19 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log("js connected");
+    initCanvas();
+    startMain();
+}
+
+function initCanvas() {
+    _$canvas = document.querySelector('canvas');
+    _ctx = _$canvas.getContext('2d');
+
+    _$canvas.width = 1075;
+    _$canvas.height = 650;
+}
+
+function startMain() {
+    const main = new Main();
+    main.run();
 }
