6f6ab17ee789025cd268a9fb77ebb54d5f33b0d1
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-06-04 15:02:19 +0200

    generating nodes

--- assets/js/field.js
+++ assets/js/field.js
@@ -2,6 +2,29 @@
 
 class Field {
     constructor() {
-        
+        this.nodeCount = {
+            horizontal: 30,
+            vertical: 10
+        }
+        this.offsetToCorner = 30;
+        this.distToBottom = 125;
+        this.nodes = [];
+        this.fill();
+    }
+
+    fill() {
+        const horDistBetween = (_$canvas.width - 2 * this.offsetToCorner) / this.nodeCount.horizontal;
+        const vertDistBetween = (_$canvas.height - 2 * this.distToBottom) / this.nodeCount.vertical;
+
+        for (let yPos = this.offsetToCorner; yPos <= _$canvas.height - this.distToBottom + 0.01; yPos += vertDistBetween) {
+            for (let xPos = this.offsetToCorner; xPos <= _$canvas.width - this.offsetToCorner + 0.01; xPos += horDistBetween) {
+                const generatedNode = new Node(xPos, yPos);
+                this.nodes.push(generatedNode);
+            }
+        }
+    }
+
+    draw() {
+        this.nodes.forEach(node => node.draw());
     }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,12 @@ class Main {
     constructor() {
         this.backgroundColor = '#8C8C8C';
         this.fps = 60;
+        this.field = new Field();
     }
 
     draw() {
         drawRect(this.backgroundColor, [0, 0, _$canvas.width, _$canvas.height]);
+        this.field.draw();
     }
 
     update() {
